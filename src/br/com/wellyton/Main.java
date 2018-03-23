be1e7db49fae8a803b12fac4eda1749d0df0d0b5
wellyton.154317 <wellyton.154317@L-1-17-06--16.satc.edu.br>
2018-03-23 17:26:37 -0300

    well

--- src/br/com/wellyton/Main.java
+++ src/br/com/wellyton/Main.java
@@ -1,3 +1,8 @@
+
+      
+                   
+            
+        
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,6 +12,8 @@ package br.com.wellyton;
 
 import br.com.wellyton.objetos.Disciplina;
 import br.com.wellyton.objetos.Professor;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,56 +21,80 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author wellyton.154317
+ * @author gabriela.154717
  */
 public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws java.text.ParseException
      */
-    public static void main(String[] args) {
-          List<Disciplina> materias = new ArrayList<>();
-   do{
-      String nome;
-    String departamento;
-    char status;
-    
-    nome = JOptionPane.showInputDialog("Informe o nome da disciplina");
-    departamento = JOptionPane.showInputDialog("Qual o departamento:");
-    status = JOptionPane.showInputDialog("Status [A] ativo [I] Inativo:").charAt(0);
-    materias.add(new Disciplina(nome,departamento,status));
-    
-   
-   }while((JOptionPane.showConfirmDialog(null,"Deseja Continuar?")) == 0);
-   String disciplinasDisponiveis = "";
-    for(Disciplina materia : materias) {
-        disciplinasDisponiveis +="\n"+ materias.lastIndexOf(materia)+ " " + materia.getNome();
+    public static void main(String[] args) throws ParseException {
+        List<Disciplina> materias = new ArrayList<>();
+        List<Professor> prof = new ArrayList<>();
+        int esc = 0;
+        do {
+            esc = Integer.parseInt(JOptionPane.showInputDialog("Escolha"
+                    + "\n 1. Cadastro de Disciplina"
+                    + "\n 2. Cadastro de Professor"
+                    + "\n 3. Cadastro de Aluno"
+                    + "\n 4. Consulta de Disciplina"
+                    + "\n 5. Consulta de Professor"
+                    + "\n 6. Consulta de Aluno"
+                    + "\n 7. Sair"));
+            switch (esc) {
+                case 1: {
+
+                    do {
+                        String nome;
+                        String departamento;
+                        char status;
+
+                        nome = JOptionPane.showInputDialog("Informe o Nome da Disciplina: ");
+                        departamento = JOptionPane.showInputDialog("Qual o Departamento? ");
+                        status = JOptionPane.showInputDialog("Status [A]tivo ou [I]nativo").charAt(0);
+                        materias.add(new Disciplina(nome, departamento, status));
+
+                    } while ((JOptionPane.showConfirmDialog(null, "Deseja Continuar?")) == 0);
+                    break;
                 }
-    JOptionPane.showMessageDialog(null, disciplinasDisponiveis);
-       
+                case 2: {
+                    do {
+                        int cargaHoraria;
+                        float valorHora;
+                        float salario = 0;
+                        String nome;
+                        String rg;
+                        String cpf;
+                        int idade = 0;
+                        Date dataNascimento;
+                        SimpleDateFormat conversor = new SimpleDateFormat("dd/MM/yyyy");
+
+                        nome = JOptionPane.showInputDialog("Informe seu Nome: ");
+                        rg = JOptionPane.showInputDialog("Informe seu RG: ");
+                        cpf = JOptionPane.showInputDialog("Informe seu CPF: ");
+                        dataNascimento = conversor.parse(JOptionPane.showInputDialog("informe a Data de Nascimento: "));
+                        cargaHoraria = Integer.parseInt(JOptionPane.showInputDialog("Informe sua carga Horaria"));
+                        valorHora = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor da hora: "));
+
+                        prof.add(new Professor(cargaHoraria, valorHora, salario, nome, rg, cpf, idade, dataNascimento));
+
+                        if (JOptionPane.showConfirmDialog(null, "Professor já trabalha em alguma disciplina") == 0);
+                        do {
+                            String disciplinaExistente = "";
+                            for (Disciplina materia : materias) {
+                                disciplinaExistente += "\n" + materias.indexOf(materias) + " " + materia.getNome();
+                            }
+                            int escolhaDisciplina = Integer.parseInt(JOptionPane.showInputDialog("Informe qual disciplina: \n" + disciplinaExistente));
+                            prof.get(prof.size() - 1).getDisciplinas().add(materias.get(escolhaDisciplina));
+                        } while ((JOptionPane.showConfirmDialog(null, "Professor possui outra disciplina?")) == 0);
+
+                    } while ((JOptionPane.showConfirmDialog(null, "Deseja Continuar?")) == 0);
+                    break;
+                }
+            }
+
+        } while (esc != 7);
     }
-
-       Date dataNascimento = new Date();
-     materias.add(new Disciplina("POO", "Informática", 'A'));
-     materias.add(new Disciplina("Redes", "Informatica" 'A'));
-     materias.add(new Disciplina("Estrutura de Dados ", "Informática" 'A'));
-     materias.add(new Disciplina("Línguagem de Programação", "Informática" 'A'));
-    
-     
-     Professor professor = new Professor(20,10,5,"professor" , "541");
-      
-     String disciplinasDisponiveis = "";
-     for (Disciplina materia : materias){
-disciplinasDisponiveis +="\n"+ materias.lastIndexOf(materia)+ " " + materia.getNome();
-}do{
-    int esc = Integer.parseInt(JOptionPane.showConfirmDialog(""));
-    professor.getDisciplinas().add(materias.get(esc));
-}while ((JOptionPane.showConfirmDialog(null,"Deseja cadastrar?"));
-JOptionPane.showMessageDialog(null, markson.getDisciplinas();
-    
-}
-
-
-
 }
 
